699f67a8302a22b2de76e05b0f9c4f9fac78d181
arun <arunsaimlai@gmail.com>
2021-10-23 00:21:33 +0530

    model saving changes added

--- src/training.py
+++ src/training.py
@@ -1,7 +1,8 @@
 from src.utils.common import read_yaml_contents
 from src.utils.load_data import get_data
-from src.utils.model import create_model
+from src.utils.model import create_model, save_model
 import argparse
+import os
 
 def training(config_path):
     config = read_yaml_contents(config_path)
@@ -19,6 +20,16 @@ def training(config_path):
     model = create_model(loss_function, metrics, optimizer)
     history = model.fit(X_train, y_train, epochs = epochs, validation_data= (X_train_val, y_train_val) )
     
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    model_dir = config['artifacts']['model_dir']
+    model_name = config['artifacts']['model_name']
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+    # saving the model
+    save_model(model, model_name, model_dir_path)
+    
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
